5ccfca0c727ef68ebf6121a705a6dac5cc06a122
AliNowrouzi <alinowrouzii@gmail.com>
2021-08-18 03:22:14 +0430

    Add auth and post route

--- routes/post.js
+++ routes/post.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const verifyToken = require('./../middleware/auth')
-
+const { verifyToken, isAdmin } = require('./../middleware/auth')
+const { createPost, getPost } = require('./../controllers/postController')
 const router = express.Router();
 
+// router.use(verifyToken);
 
-router.get('/',verifyToken, (req, res) => {
+router.get('/test', isAdmin, (req, res) => {
     // console.log(req.user);
     // console.log('token:',req.cookies.token);
     res.status(200).send('hello');
-})
+});
+
+router.post('/createPost', createPost);
+// router.post('/createPost', isAdmin, createPost);
+router.get('/getPost', getPost);
 
 module.exports = router;

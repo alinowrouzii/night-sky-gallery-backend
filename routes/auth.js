54926e0d0b5fd4abd133f27fb056db962939804b
AliNowrouzi <alinowrouzii@gmail.com>
2021-08-28 05:17:37 +0430

    Add confirmUser route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { verifyToken } = require('../middleware/auth.js');
-const { register, login, logout } = require('./../controllers/authController.js')
+const { register, login, logout, confirmUser } = require('./../controllers/authController.js')
 
 const router = express.Router();
 
 router.post('/register', register);
+router.post('/confirmUser', confirmUser);
 router.post('/login', login);
 router.get('/logout', verifyToken, logout);
 

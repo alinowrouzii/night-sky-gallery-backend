f3edeb4c13c7e9e04e4ab59c35f50860f78c1900
AliNowrouzi <alinowrouzii@gmail.com>
2021-08-30 17:33:36 +0430

    Add superAdmin route

---
+++ routes/superAdmin.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { verifyToken, isSuperAdmin } = require('./../middleware/auth');
+const { fetchAdmins, verifyAdmin } = require('./../controllers/superAdminController');
+const router = express.Router();
+
+router.use([verifyToken, isSuperAdmin]);
+
+router.post('/verifyAdmin', verifyAdmin);
+router.get('/', fetchAdmins);
+
+module.exports = router;

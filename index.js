f1215fcf1368a40bf8429b3a69da7473b62c1379
AliNowrouzi <alinowrouzii@gmail.com>
2021-08-30 23:50:14 +0430

    Add mongooseSetup and app

--- index.js
+++ index.js
@@ -1,42 +1,18 @@
-require('dotenv').config();
-
-const logger = require('./config/logger');
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser')
-const userRoute = require('./routes/user.js');
-const postRoute = require('./routes/post.js');
-const authRoute = require('./routes/auth.js');
-const adminRoute = require('./routes/admin.js');
-const superAdminRouter = require('./routes/superAdmin.js')
-const app = express();
-
-
-app.use(express.json({ limit: '20mb', extended: true }))
-app.use(express.urlencoded({ limit: '20mb', extended: true }))
-app.use(cors({
-    // origin: '*',
-    origin: 'http://localhost:3000',
-    credentials: true,
-}));
-
-app.use(cookieParser());
 
 
 //mongoDB connection setup
+const logger = require('./config/logger.js');
 const mongooseSetup = require('./config/mongooseSetup.js');
-mongooseSetup();
+const app = require('./app');
 
 
-// app.get('/',(req,res)=>{
-//     console.log('hello')
-// })
+mongooseSetup().then(() => {
+    logger.info('MongoDB connected!!');
 
-app.use('/user', userRoute);
-app.use('/post', postRoute);
-app.use('/auth', authRoute);
-app.use('/admin', adminRoute);
-app.use('/superAdmin', superAdminRouter);
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => logger.info(`Server is running on Port: ${PORT}`));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => logger.info(`Server is running on Port: ${PORT}`));
+}).catch(err => {
+    logger.error(`${err} mongoDB didn't connect!`);
+    process.exit(1)
+});

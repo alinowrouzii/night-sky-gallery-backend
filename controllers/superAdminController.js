ed743b4df8690bdef1ab2e8c1e13421c9ea41ad8
AliNowrouzi <alinowrouzii@gmail.com>
2021-08-30 17:31:20 +0430

    Add fetchAdmins

--- controllers/superAdminController.js
+++ controllers/superAdminController.js
@@ -1,79 +1,71 @@
 const mongoose = require("mongoose");
 const User = require("./../models/User.js");
 const { userTypes, userStatus } = require("./../models/constants.js");
+const logger = require('./../config/logger');
 
-// exports.login = async (req, res) => {
+exports.verifyAdmin = async (req, res) => {
 
-//     const { password } = req.body;
+    const { username } = req.body;
 
-//     if (!password) {
-//         return res.status(400).send("password is required");
-//     }
-
-//     try {
-//         const fetchedUser = await User.findOne({
-//             username: userTypes.SUPER_ADMIN.toLowerCase()
-//         }).select('+salt').select('+hash').lean();
-
-//         if (!fetchedUser) {
-//             return res.status(400).json({ message: 'User not found!' });
-//         }
-//         const { hash, salt, ...user } = fetchedUser;
-
-//         if (user.role === userTypes.SUPER_ADMIN) {
-
-//             if (validatePassword(password, hash, salt)) {
-
-//                 const token = jwt.sign({ user_id: user._id, username, role: user.role }
-//                     , TOKEN_KEY, {
-//                     algorithm: "HS256",
-//                     expiresIn: jwtExpirySeconds,
-//                 });
-
-//                 res.cookie("token", token, { maxAge: jwtExpirySeconds * 1000 });
-
-//                 return res.status(200).json({ user, message: 'You have been successfully logged in as superUser' });
-//             }
-//             return res.status(401).json({ message: 'Wrong password' });
-//         } else {
-//             return res.status(401).json({ message: 'Only super admin can get access!' });
-//         }
-//     } catch (err) {
-//         console.log(err);
-//         return res.status(502).json({ message: 'Database error!' });
-//     }
-// }
-
-exports.acceptAdmin = async (req, res) => {
-
-    const { userId } = req.body;
-
-    if (!userId) {
-        return res.status(400).json({ message: 'username and password are required!' });
+    if (!username) {
+        return res.status(400).json({ message: 'username is required!' });
     }
 
-
     try {
-        const Id = await mongoose.Types.ObjectId(userId);
 
-        const fetchedUser = await User.findById(Id);
+        const fetchedUser = await User.findOne({ username, role: userTypes.ADMIN });
 
         if (!fetchedUser) {
             return res.status(404).json({ message: 'User not found!' });
         }
 
         if (fetchedUser.status === userStatus.SUPER_ADMIN_PENDING) {
+
             fetchedUser.status = userStatus.ACCEPTED;
             await fetchedUser.save();
-            return res.status()
+            return res.status(200).json({ message: 'User activated successully!' })
         }
 
-
+        return res.status(400).json({ message: 'Bad request!' });
 
 
     } catch (err) {
-        console.log(err.message || err.msg || err);
+        logger.error(err.message || err.msg || err);
         return res.status(503).json({ message: 'Something went wrong with Database!' });
     }
 
+}
+
+exports.fetchAdmins = async (req, res) => {
+
+    const { filter } = req.body;
+
+    try {
+
+        if (filter === userStatus.ACCEPTED) {
+
+            const users = await User.find({
+                status: userStatus.ACCEPTED,
+                role: userTypes.ADMIN
+            });
+            return res.status(200).json({ users, });
+        }
+        else if (filter === userStatus.SUPER_ADMIN_PENDING) {
+
+            const users = await User.find({
+                status: userStatus.SUPER_ADMIN_PENDING,
+                role: userTypes.ADMIN
+            });
+            return res.status(200).json({ users, });
+        }
+        else {
+            const users = await User.find({
+                role: userTypes.ADMIN
+            });
+            return res.status(200).json({ users, });
+        }
+    } catch (err) {
+        logger.error(err.message || err.msg || err);
+        return res.status(503).json({ message: 'Something went wrong with Database!' });
+    }
 }

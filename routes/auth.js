1bb80c30258036f64232af40628ca452a36066e4
AliNowrouzi <alinowrouzii@gmail.com>
2021-09-01 23:46:08 +0430

    Add sendCode to admin router

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { verifyToken } = require('../middleware/auth.js');
-const { register, login, logout, confirmUser } = require('./../controllers/authController.js')
+const { register, login, logout, confirmUser, sendConfirmationCode } = require('./../controllers/authController.js')
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/confirmUser', confirmUser);
+router.post('/sendCode', sendConfirmationCode);
 router.post('/login', login);
 router.get('/logout', verifyToken, logout);
 

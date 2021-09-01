bc64a6b793d8305bd479a83dbe6b52095df4f1b6
AliNowrouzi <alinowrouzii@gmail.com>
2021-09-01 23:49:33 +0430

    res.setHeader('Content-disposition', 'attachment; filename=' + photoName);Wrap index code inside run api for trying more than once for connnecting to DB

--- index.js
+++ index.js
@@ -5,14 +5,28 @@ const logger = require('./config/logger.js');
 const mongooseSetup = require('./config/mongooseSetup.js');
 const app = require('./app');
 
+let attempt = 0;
+const MAX_ATTEMPT = 5;
 
-mongooseSetup().then(() => {
-    logger.info('MongoDB connected!!');
+const runApi = () => {
+    attempt += 1;
 
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => logger.info(`Server is running on Port: ${PORT}`));
+    logger.info('Try to run API');
+    mongooseSetup().then(() => {
+        logger.info('MongoDB connected!!');
 
-}).catch(err => {
-    logger.error(`${err} mongoDB didn't connect!`);
-    process.exit(1)
-});
+        const PORT = process.env.PORT || 5000;
+        app.listen(PORT, () => logger.info(`Server is running on Port: ${PORT}`));
+
+    }).catch(err => {
+        logger.error(`${err} mongoDB didn't connect!`);
+        if (attempt < MAX_ATTEMPT) {
+            setTimeout(runApi, 10000);
+        } else {
+            logger.error('Exiting the process!');
+            process.exit(1)
+        }
+    });
+}
+
+runApi();

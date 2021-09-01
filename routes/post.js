f2e5a7ff623fad02839175fb1f1d70300f51f5b7
AliNowrouzi <alinowrouzii@gmail.com>
2021-09-01 23:48:00 +0430

    some changes

--- routes/post.js
+++ routes/post.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const { verifyToken, isAdmin } = require('./../middleware/auth')
-const { createPost, getPost } = require('./../controllers/postController')
+const { verifyToken } = require('./../middleware/auth')
+const { getPosts, downloadPhoto } = require('./../controllers/postController')
 const router = express.Router();
 
+//TODO: check that unauthorized users can see the posts or not
 // router.use(verifyToken);
 
-router.get('/test', isAdmin, (req, res) => {
-    // console.log(req.user);
-    // console.log('token:',req.cookies.token);
-    res.status(200).send('hello');
-});
-
-router.post('/createPost', createPost);
-// router.post('/createPost', isAdmin, createPost);
-router.get('/getPost', getPost);
+router.get('/', getPosts);
+router.get('/donwloadPhoto/:photoName', downloadPhoto);
 
 module.exports = router;

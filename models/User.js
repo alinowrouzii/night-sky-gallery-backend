3ff6e16e85db817b093452374f2ea937d21c2e9c
AliNowrouzi <alinowrouzii@gmail.com>
2021-09-01 23:37:01 +0430

    Set select of some fields to false and create static method for user to select all fields

--- models/User.js
+++ models/User.js
@@ -18,16 +18,19 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
+        select: false
     },
     role: {
         type: String,
         required: true,
-        enum: [userTypes.SUBSCRIBER, userTypes.ADMIN, userTypes.SUPER_ADMIN]
+        enum: [userTypes.SUBSCRIBER, userTypes.ADMIN, userTypes.SUPER_ADMIN],
+        select: false
     },
     status: {
         type: String,
         required: true,
-        enum: [userStatus.PENDING, userStatus.SUPER_ADMIN_PENDING, userStatus.ACCEPTED, userStatus.REJECTED]
+        enum: [userStatus.PENDING, userStatus.SUPER_ADMIN_PENDING, userStatus.ACCEPTED, userStatus.REJECTED],
+        select: false
     },
     confirmation: {
         type: {
@@ -49,6 +52,11 @@ const userSchema = new mongoose.Schema({
     },
 }, { timestamps: true });
 
+
+//Selects all fields except confirmation
+userSchema.statics.findOneAndSelectAll = function (user) {
+    return this.findOne(user).select('+hash').select('+salt').select('+status').select('+role').select('+phoneNumber');
+}
 
 const User = mongoose.model('User', userSchema);
 

cdca10545e31e248af2fbe0fad1c1bf867688800
AliNowrouzi <alinowrouzii@gmail.com>
2021-09-01 23:44:03 +0430

    Add createPost to admin router

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,12 @@
 const express = require('express');
-
+const { verifyToken, isAdmin } = require('./../middleware/auth');
+const { createPost } = require('./../controllers/postController');
 
 const router = express.Router();
 
+router.use([verifyToken, isAdmin]);
 
-
-
-
-
+router.post('/', createPost);
 
 
 module.exports = router;

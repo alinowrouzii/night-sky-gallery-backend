68faf5620bc4e7deb42bcae03dba0df84e6894fe
AliNowrouzi <alinowrouzii@gmail.com>
2021-09-03 23:55:46 +0430

    Add swagger /api-docs to app

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 
-const logger = require('./config/logger');
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser')
@@ -8,9 +7,11 @@ const userRoute = require('./routes/user.js');
 const postRoute = require('./routes/post.js');
 const authRoute = require('./routes/auth.js');
 const adminRoute = require('./routes/admin.js');
-const superAdminRouter = require('./routes/superAdmin.js')
-const app = express();
+const superAdminRouter = require('./routes/superAdmin.js');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 
+const app = express();
 
 app.use(express.json({ limit: '20mb', extended: true }))
 app.use(express.urlencoded({ limit: '20mb', extended: true }))
@@ -32,6 +33,12 @@ app.use('/post', postRoute);
 app.use('/auth', authRoute);
 app.use('/admin', adminRoute);
 app.use('/superAdmin', superAdminRouter);
+
+var options = {
+    explorer: true
+  };
+const swaggerDocument = YAML.load('./swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 
 module.exports = app;

b7ee07bbac80c603ad848bc400f44f635deb7b07
AliNowrouzi <alinowrouzii@gmail.com>
2021-09-07 03:16:00 +0430

    Remove options from mongoose because of deprecation in mongo new version

--- config/mongooseSetup.js
+++ config/mongooseSetup.js
@@ -7,32 +7,27 @@ module.exports = async () => {
     if (process.env.NODE_ENV === 'dev') {
         MONGOOSE_URL = "mongodb://localhost:27017/devTestDB";
 
-        mongoose.connect(MONGOOSE_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        }).then(() => {
-            logger.info('MongoDB connected for dropping');
+        // mongoose.connect(MONGOOSE_URL, {
+        //     useNewUrlParser: true,
+        //     useUnifiedTopology: true,
+        //     useCreateIndex: true,
+        //     useFindAndModify: false
+        // }).then(() => {
+        //     logger.info('MongoDB connected for dropping');
 
-            mongoose.connection.db.dropDatabase(() => {
-                logger.info('Droping Database!');
-            });
-        }).catch(error => {
-            logger.error(`Faild to connect mongoDB for auth test: ${error}`);
-        });
+        //     mongoose.connection.db.dropDatabase(() => {
+        //         logger.info('Droping Database!');
+        //     });
+        // }).catch(error => {
+        //     logger.error(`Faild to connect mongoDB for auth test: ${error}`);
+        // });
 
     } else {
         MONGOOSE_URL = process.env.MONGOOSE_URL;
     }
 
     try {
-        await mongoose.connect(MONGOOSE_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        });
+        await mongoose.connect(MONGOOSE_URL);
         return Promise.resolve();
     } catch (error) {
         return Promise.reject(error);

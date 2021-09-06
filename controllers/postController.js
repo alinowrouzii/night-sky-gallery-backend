d4ca3ea5687df179730bf89cd4a3e2307fc5794a
AliNowrouzi <alinowrouzii@gmail.com>
2021-09-07 03:17:45 +0430

    Add addNewComment handler

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,199 +1,19 @@
-
-const { uploadPath, maximumFileSize } = require('./constants');
 const path = require('path');
 const mime = require('mime-types');
 const fse = require('fs-extra');
-const mongoose = require('mongoose');
-const multer = require('multer');
-const multerConfig = require('./../config/multer');
 const Post = require('../models/Post');
 const validator = require('validator');
 
 const logger = require('../config/logger');
-
-exports.createPost = async (req, res) => {
-
-    //TODO: specify what types of file should be uploaded
-    const validFileTypes = ['png', 'jpeg', 'jpg'];
-    const upPath = path.join(uploadPath, 'posts/photos')
-    const upload = multerConfig(upPath, maximumFileSize, validFileTypes);
-
-    upload.single('photo')(req, res, async function (err) {
-        if (err) {
-            if (err instanceof multer.MulterError) {
-                if (err.code === 'LIMIT_FILE_SIZE') {
-                    return res.status(413).json({ message: `File is larger than ${maximumFileSize / (1024 * 1024)}MB` });
-                }
-            }
-            return res.status(406).json({ message: err.message || err.msg || 'Upload failed' });
-        }
-
-        if (!req.file) {
-            return res.status(500).json({ message: 'Upload failed!' });
-        }
-
-        const { title, caption } = req.body;
-        if (!(title && caption)) {
-            return res.status(400).json({ message: 'all input is required!' });
-        }
-
-        try {
-            //post fields:
-            // title
-            // caption
-            // photo
-            // comments
-            // creator
-            const photoName = req.file.filename;
-            const creator = req.user.user_id;
-            const post = new Post({
-                title, caption, creator,
-                photo: photoName
-            });
-
-            await post.save();
-
-            logger.info('post created successfuly!')
-            return res.status(201).json({ post, message: 'post uploaded successfully!' });
-        } catch (err) {
-            logger.error(err.message);
-            return res.status(500).json({ message: `Database error: ${err.message}` });
-        }
-    });
-}
-
-
-exports.editPost = async (req, res) => {
-
-    const { fieldsToUpdate, postId } = req.body;
-
-    if (!(fieldsToUpdate && postId)) {
-        return res.status(400).json({ message: 'fieldsToUpdate and postId are required' });
-    }
-
-    const { caption, title } = fieldsToUpdate;
-
-    if (!(caption || title)) {
-        return res.status(400).json({ message: 'fieldsToUpdate is required' });
-    }
-
-    if (!validator.isMongoId(postId)) {
-        return res.status(400).json({ message: 'postId is not valid' });
-    }
-
-    try {
-
-        const id = mongoose.Types.ObjectId(postId);
-        const post = await Post.findOneAndSelectAll({ _id: id });
-        if (!post) {
-            return res.status(404).end();
-        }
-
-        if (caption) {
-            post.caption = caption;
-        }
-        if (title) {
-            post.title = title;
-        }
-
-        await post.save();
-
-        return res.status(200).json({ post: post.toObject() });
-    } catch (err) {
-        logger.error(err.msg || err.message || JSON.stringify(err));
-        res.status(500).json({ message: 'Database error!' });
-    }
-}
-
-exports.editPostPhoto = async (req, res) => {
-
-    const { postId } = req.params;
-    if (!postId) {
-        return res.status(400).json({ message: 'postId is required' });
-    }
-
-    if (!validator.isMongoId(postId)) {
-        return res.status(400).json({ message: 'postId is not valid' });
-    }
-    try {
-        const id = mongoose.Types.ObjectId(postId)
-        const post = await Post.findOneAndSelectAll({ _id: id });
-
-        if (!post) {
-            return res.status(404).end();
-        }
-
-        const validFileTypes = ['png', 'jpeg'];
-        const upPath = path.join(uploadPath, 'posts/photos')
-        const upload = multerConfig(upPath, maximumFileSize, validFileTypes);
-
-        upload.single('photo')(req, res, async function (err) {
-            if (err) {
-                if (err instanceof multer.MulterError) {
-                    if (err.code === 'LIMIT_FILE_SIZE') {
-                        return res.status(413).json({ message: `File is larger than ${maximumFileSize / (1024 * 1024)}MB` });
-                    }
-                }
-                return res.status(406).json({ message: err.message || err.msg || 'Upload failed' });
-            }
-
-            if (!req.file) {
-                return res.status(500).json({ message: 'Upload failed!' });
-            }
-
-            //means successfull upload!
-            logger.info('file uploaded!');
-
-            const photoName = req.file.filename;
-
-            //push current post photo into prevPhotos and then update the current photo
-            post.prevPhotos.push(post.photo);
-
-            post.photo = photoName;
-            await post.save();
-
-            return res.status(200).json({ post: post.toObject() });
-        })
-    } catch (err) {
-        logger.error(err.message || err.msg || err);
-        return res.status(500).json({ message: 'Database error!' });
-    }
-}
-
-
-exports.deleteOnePost = async (req, res) => {
-
-    const { postId } = req.params;
-
-    if (!postId) {
-        return res.status(400).json({ message: 'postId is required!' });
-    }
-
-    if (!validator.isMongoId(postId)) {
-        return res.status(400).json({ message: 'postId is not valid!' });
-    }
-
-    try {
-        const deletedPost = await Post.findByIdAndDelete(postId);
-
-        if(!deletedPost){
-            return res.status(404).json({message: 'Post not found!'});
-        }
-        return res.status(200).json({ message: 'Post deleted successfully!' });
-
-    } catch (err) {
-        logger.error(err.message || err.msg || JSON.stringify(err))
-        return res.status(500).json({ message: 'Database error!' });
-    }
-}
-
+const { CommentStatus } = require('../models/constants');
+const Comment = require('../models/Comment');
 
 exports.getPosts = async (req, res) => {
 
     try {
 
         //TODO: add some limitation to fetch posts
-        const posts = await Post.find().lean();
+        const posts = await Post.findAndPapulate();
 
         return res.status(200).json({ posts, });
     } catch (err) {
@@ -229,8 +49,47 @@ exports.downloadPhoto = (req, res) => {
             return res.status(500).json({ message: 'Something went wrong!' });
         }
     })
+}
 
 
+exports.addCommentToPost = async (req, res) => {
 
+    const { postId } = req.params;
 
+    if (!postId) {
+        return res.status(400).json({ message: 'post id is required!' });
+    }
+    if (!validator.isMongoId(postId)) {
+        return res.status(400).json({ message: 'Post id is not valid' });
+    }
+
+    const { text } = req.body;
+
+    if (!text) {
+        return res.status(400).json({ message: 'text body is required!' });
+    }
+
+    try {
+        //TODO: check that string postId accepted or not
+        const post = await Post.findById(postId);
+        if (!post) {
+            return res.status(404).json({ message: 'Post not found!' });
+        }
+
+        const user = req.user._id;
+        const comment = new Comment({
+            text, user,
+            status: CommentStatus.PENDING,
+            post: postId
+        });
+        await comment.save();
+
+        post.comments.push(comment._id);
+        await post.save();
+
+        res.status(201).json({ message: 'Comment submitted! wait for confirmation' });
+    } catch (err) {
+        logger.error(err.message || err.msg || JSON.stringify(err));
+        return res.status(500).json({ message: err.message || err.msg || 'Database error' });
+    }
 }

1a599b56b42e156fabe74ab6e4298bdc6194ffb9
AliNowrouzi <alinowrouzii@gmail.com>
2021-09-07 03:14:55 +0430

    Add new static method to schema

--- models/Post.js
+++ models/Post.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const { CommentStatus } = require('./constants');
+
 const postSchema = new mongoose.Schema({
     title: {
         type: String,
@@ -36,6 +38,27 @@ postSchema.statics.findOneAndSelectAll = function (post) {
     return this.findOne(post).select('+prevPhotos').select('+creator');
 }
 
+
+
+postSchema.statics.findAndPapulate = function (filter) {
+
+    return this.find(filter).populate(
+        {
+            path: 'comments',
+            match: {
+                //to only return ACCEPTED comment
+                status: CommentStatus.ACCEPTED
+            },
+            //TODO: add status field to be exclUded
+            select: '-post -__v',
+
+            populate: {
+                path: 'user',
+                select: '+firstName +lastName +username -_id -__v -createdAt -updatedAt'
+            }
+        }
+    ).select('-__v').lean()
+}
 
 const Post = mongoose.model('Post', postSchema);
 

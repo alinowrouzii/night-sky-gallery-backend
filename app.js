df61afaa858c5c951942dd166e1f6d01801ec427
AliNowrouzi <alinowrouzii@gmail.com>
2021-09-07 03:18:48 +0430

    rename admin router path

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ const app = express();
 app.use(express.json({ limit: '20mb', extended: true }))
 app.use(express.urlencoded({ limit: '20mb', extended: true }))
 app.use(cors({
-    // origin: '*',
-    origin: 'http://localhost:3000',
-    credentials: true,
+  // origin: '*',
+  origin: 'http://localhost:3000',
+  credentials: true,
 }));
 
 app.use(cookieParser());
@@ -31,12 +31,12 @@ app.use(cookieParser());
 app.use('/user', userRoute);
 app.use('/post', postRoute);
 app.use('/auth', authRoute);
-app.use('/admin', adminRoute);
+app.use('/admin/post', adminRoute);
 app.use('/superAdmin', superAdminRouter);
 
 var options = {
-    explorer: true
-  };
+  explorer: true
+};
 const swaggerDocument = YAML.load('./swagger.yaml');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 

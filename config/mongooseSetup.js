da3674e00ca7bbf18e13fab297bde77251a58c8c
alinowrouzii <alinowrouzii@gmail.com>
2021-09-09 23:49:55 +0430

    Add some env vars

--- config/mongooseSetup.js
+++ config/mongooseSetup.js
@@ -6,35 +6,19 @@ const {
     MONGO_PASSWORD,
     MONGO_IP,
     MONGO_PORT,
-  } = require("./index");
+} = require("./index");
 
 module.exports = async () => {
-    const MONGOOSE_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/testDev/?authSource=admin`;
-
-    // if (process.env.NODE_ENV === 'development') {
-    //     MONGOOSE_URL = "mongodb://localhost:27017/devTestDB";
-
-    //     // mongoose.connect(MONGOOSE_URL, {
-    //     //     useNewUrlParser: true,
-    //     //     useUnifiedTopology: true,
-    //     //     useCreateIndex: true,
-    //     //     useFindAndModify: false
-    //     // }).then(() => {
-    //     //     logger.info('MongoDB connected for dropping');
-
-    //     //     mongoose.connection.db.dropDatabase(() => {
-    //     //         logger.info('Droping Database!');
-    //     //     });
-    //     // }).catch(error => {
-    //     //     logger.error(`Faild to connect mongoDB for auth test: ${error}`);
-    //     // });
-
-    // } else {
-    //     MONGOOSE_URL = process.env.MONGOOSE_URL;
-    // }
+    //TODO: just for test and dev
+    let MONGO_URL;
+    if (process.env.NODE_ENV === 'dev') {
+        MONGO_URL = `mongodb://localhost:27017/testDev`
+    } else {
+        MONGO_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/testDev?authSource=admin`;
+    }
 
     try {
-        await mongoose.connect(MONGOOSE_URL);
+        await mongoose.connect(MONGO_URL);
         return Promise.resolve();
     } catch (error) {
         return Promise.reject(error);

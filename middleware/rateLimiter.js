70f24c3251228bf9d5d5da7a2631da8a69b0406a
alinowrouzii <alinowrouzii@gmail.com>
2021-09-12 19:02:06 +0430

    Update duration

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -1,28 +1,29 @@
-const redis = require('redis');
-const { RateLimiterRedis } = require('rate-limiter-flexible');
+const redis = require("redis");
+const { RateLimiterRedis } = require("rate-limiter-flexible");
 
 const redisClient = redis.createClient({
-    host: '127.0.0.1',
-    port: 6379,
-    enable_offline_queue: false,
+  host: "127.0.0.1",
+  port: 6379,
+  enable_offline_queue: false,
 });
 
 const rateLimiter = new RateLimiterRedis({
-    storeClient: redisClient,
-    keyPrefix: 'middleware',
-    points: 1, // 1 requests
-    //TODO: update after testing
-    duration: 500, // per 5 second by IP
+  storeClient: redisClient,
+  keyPrefix: "middleware",
+  points: 1, // 1 requests
+  //TODO: update after testing
+  duration: 1, // per 1 second by IP
 });
 
 const rateLimiterMiddleware = (req, res, next) => {
-    rateLimiter.consume(req.ip)
-        .then(() => {
-            next();
-        })
-        .catch(() => {
-            res.status(429).send('Too Many Requests');
-        });
+  rateLimiter
+    .consume(req.ip)
+    .then(() => {
+      return next();
+    })
+    .catch(() => {
+      return res.status(429).send("Too Many Requests");
+    });
 };
 
 module.exports = rateLimiterMiddleware;

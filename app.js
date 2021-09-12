d9e92aeb12e56cf7955c7884e7d572ef1c4527f3
alinowrouzii <alinowrouzii@gmail.com>
2021-09-12 19:28:02 +0430

    remove swagger from limitter

--- app.js
+++ app.js
@@ -1,47 +1,53 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors'); 
-const cookieParser = require('cookie-parser')
-const userRoute = require('./routes/user.js');
-const postRoute = require('./routes/post.js');
-const authRoute = require('./routes/auth.js');
-const adminRoute = require('./routes/admin.js');
-const superAdminRouter = require('./routes/superAdmin.js');
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const rateLimiterMiddleware = require('./middleware/rateLimiter.js');
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const userRoute = require("./routes/user.js");
+const postRoute = require("./routes/post.js");
+const authRoute = require("./routes/auth.js");
+const adminRoute = require("./routes/admin.js");
+const superAdminRouter = require("./routes/superAdmin.js");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const rateLimiterMiddleware = require("./middleware/rateLimiter.js");
 
 const app = express();
 
-app.use(express.json({ limit: '20mb', extended: true }))
-app.use(express.urlencoded({ limit: '20mb', extended: true }))
-app.use(cors({
-  // origin: '*',
-  origin: 'http://localhost:3000',
-  credentials: true,
-}));
+app.use(express.json({ limit: "20mb", extended: true }));
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
+app.use(
+  cors({
+    // origin: '*',
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
-app.use(rateLimiterMiddleware);
 
-app.set('trust proxy');
-
-app.get('/ping',(req,res)=>{
-    res.send('pong')
-})
-
-app.use('/user', userRoute);
-app.use('/post', postRoute);
-app.use('/auth', authRoute);
-app.use('/admin/post', adminRoute);
-app.use('/superAdmin', superAdminRouter);
+app.set("trust proxy");
 
 var options = {
-  explorer: true
+  explorer: true,
 };
-const swaggerDocument = YAML.load('./swagger.yaml');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+const swaggerDocument = YAML.load("./swagger.yaml");
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, options)
+);
 
+app.use(rateLimiterMiddleware);
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+app.use("/user", userRoute);
+app.use("/post", postRoute);
+app.use("/auth", authRoute);
+app.use("/admin/post", adminRoute);
+app.use("/superAdmin", superAdminRouter);
 
 module.exports = app;

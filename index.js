ec465b44925c0656aa16b5d7c7ac96c9a7307429
alinowrouzii <alinowrouzii@gmail.com>
2021-09-15 04:10:46 +0430

    Some changes

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-
-
 //mongoDB connection setup
-const logger = require('./config/logger.js');
-const mongooseSetup = require('./config/mongooseSetup.js');
-const app = require('./app');
+const logger = require("./config/logger.js");
+const mongooseSetup = require("./config/mongooseSetup.js");
+const app = require("./app");
 
 let attempt = 0;
 const MAX_ATTEMPT = 5;
 
 const runApi = () => {
-    attempt += 1;
+  attempt += 1;
 
-    logger.info('Try to run API');
-    mongooseSetup().then(() => {
-        logger.info('MongoDB connected!!');
+  logger.info("Try to run API");
+  mongooseSetup()
+    .then(() => {
+      logger.info("MongoDB connected!!");
 
-        const PORT = process.env.PORT || 5000;
-        app.listen(PORT, () => logger.info(`Server is running on Port: ${PORT}`));
-
-    }).catch(err => {
-        logger.error(`${err} mongoDB didn't connect!`);
-        if (attempt < MAX_ATTEMPT) {
-            setTimeout(runApi, 10000);
-        } else {
-            logger.error('Exiting the process!');
-            process.exit(1)
-        }
+      const PORT = process.env.PORT || 5000;
+      app.listen(PORT, () => logger.info(`Server is running on Port: ${PORT}`));
+    })
+    .catch((err) => {
+      logger.error(`${err} mongoDB didn't connect!`);
+      if (attempt < MAX_ATTEMPT) {
+        setTimeout(runApi, 10000);
+      } else {
+        logger.error("Exiting the process!");
+        process.exit(1);
+      }
     });
-}
+};
 
 runApi();

4f307de3284e5687e6093c5dff4169ca5fae15fc
alinowrouzii <alinowrouzii@gmail.com>
2021-09-15 05:56:15 +0430

    Add redis to docker

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -1,9 +1,17 @@
 const redis = require("redis");
 const { RateLimiterRedis } = require("rate-limiter-flexible");
+const { REDIS_PORT, REDIS_URL } = require("../config");
+
+let host;
+if (process.env.NODE_ENV === 'dev') {
+  host = "127.0.0.1";
+} else {
+  host = REDIS_URL
+}
 
 const redisClient = redis.createClient({
-  host: "127.0.0.1",
-  port: 6379,
+  host,
+  port: REDIS_PORT,
   enable_offline_queue: false,
 });
 

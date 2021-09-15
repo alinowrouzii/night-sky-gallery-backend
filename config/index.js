392936411703182b58c0bdd7378a38a627f89cfd
alinowrouzii <alinowrouzii@gmail.com>
2021-09-15 05:56:42 +0430

    Add redis host and port

--- config/index.js
+++ config/index.js
@@ -5,4 +5,6 @@ module.exports = {
   MONGO_PORT: process.env.MONGO_PORT || 27017,
   MONGO_USER: process.env.MONGO_USER,
   MONGO_PASSWORD: process.env.MONGO_PASSWORD,
+  REDIS_URL: process.env.REDIS_URL || "redis",
+  REDIS_PORT: process.env.REDIS_PORT || 6379,
 };

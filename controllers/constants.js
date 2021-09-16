3c6140650b88eba97086343b0945cc161c90cb8d
alinowrouzii <alinowrouzii@gmail.com>
2021-09-16 20:46:15 +0430

    Add build to nginx

--- controllers/constants.js
+++
@@ -1,9 +0,0 @@
-
-const path = require('path');
-exports.jwtExpirySeconds = 300;
-
-exports.uploadPath = path.join(__dirname, '..', 'uploads');
-
-
-//maximum size = 6MB
-exports.maximumFileSize = 6 * 1024 * 1024;

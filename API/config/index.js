9280df84540ced2a3d73862630a8f89f6bfa53c4
alinowrouzii <alinowrouzii@gmail.com>
2021-09-23 01:02:41 +0330

    Add mongo_url

--- API/config/index.js
+++ API/config/index.js
@@ -1,10 +1,24 @@
 require('dotenv').config()
 
+const { NODE_ENV } = process.env;
+let MONGO_URL;
+
+if (NODE_ENV === 'dev') {
+  MONGO_URL = `mongodb://localhost:27017/testDev`
+
+} else if (NODE_ENV === 'production' || NODE_ENV === 'development') {
+  const MONGO_IP = process.env.MONGO_IP || "mongo",
+    MONGO_PORT = process.env.MONGO_PORT || 27017,
+    MONGO_USER = process.env.MONGO_USER,
+    MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+
+  MONGO_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/testDev?authSource=admin`;
+} else {
+  throw new Error('NODE_ENV should be set!');
+}
+
 module.exports = {
-  MONGO_IP: process.env.MONGO_IP || "mongo",
-  MONGO_PORT: process.env.MONGO_PORT || 27017,
-  MONGO_USER: process.env.MONGO_USER,
-  MONGO_PASSWORD: process.env.MONGO_PASSWORD,
+  MONGO_URL,
   REDIS_URL: process.env.REDIS_URL || "redis",
   REDIS_PORT: process.env.REDIS_PORT || 6379,
 };

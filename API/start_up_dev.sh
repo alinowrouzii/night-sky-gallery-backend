298ceaa66ec71f707b0264ad8e851a4ef4fd7950
alinowrouzii <alinowrouzii@gmail.com>
2021-09-23 04:36:11 +0330

    Add script to run databases&&API

---
+++ API/start_up_dev.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+FILE_PATH="db/logfile.log"
+if [ ! -f "$FILE_PATH" ]; then
+  mkdir -p "${FILE_PATH%/*}" && touch "$FILE_PATH"
+fi
+# mongod --shutdown
+
+mongod --fork --logpath db/logfile.log --logappend
+sudo service redis-server start
+nodemon index.js
